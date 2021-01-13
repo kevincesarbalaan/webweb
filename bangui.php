bd3984f3b3486eab7213b69f8bbf31d13c74a9bb
kevincesarbalaan <60724351+kevincesarbalaan@users.noreply.github.com>
2021-01-13 12:32:18 +0800

    Add files via upload

---
+++ bangui.php
@@ -0,0 +1,38 @@
+<Doctype html>
+<html>
+<head>
+    <title>My Webpage</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+        body{
+            background-color: dimgrey;
+            text-align: center;
+        }
+    
+    
+    
+    </style>
+</head>    
+<body>
+    <br>
+    <h1>Ilocos Norte Tourist Spot</h1>
+
+    <div class="nav_bar">
+       <ul>
+           <li><a href="bagui.html" id="onlink">Bangui</a></li>
+           <li><a href="pagudpud.html">Pagudpud</a></li>
+           <li><a href="paoay.html">Paoay</a></li>
+           <li><a href="vintar.html">Vintar</a></li>
+           <li><a href="badoc.html">Badoc</a></li>
+           <li><a href="contact.html">contact us</a></li>
+       </ul>
+    </div>
+    <div class="main_container">
+        <img src="1.jpg" style="align-content: center">
+        
+        <p>Bangui Windmills</p>
+    <p>In 1996, the National Renewable Energy Laboratory (NREL) conducted a wind resource analysis and mapping study. It concluded that various areas in the Philippines are ideal for wind power installation. These areas include Bangui and Burgos towns in Ilocos Norte, Batanes and Babuyan Islands, which are north of Luzon and the higher interior terrain of Mindoro, Samar, Leyte, Panay, Negros, Cebu, Palawan and Eastern Mindanao. The study led to the inception of the wind farm project. NorthWind Power Development Corp. developed (and maintains and operates) the project, while Vestas Wind Systems, a Danish firm, supplied the wind turbine-generator units (WTGs) for the site, similar to those already found in Denmark. The project was to have been developed in three phases, with Phase I of the project inaugurated on June 18, 2005 and attended by Former First Lady Imelda Marcos, then Governor Bongbong Marcos, Former DOE Secretary Vincent Perez, Undersecretary Peter Abaya and Dr. Robert Yap, Jesuit priest and Project Director of CD4CDM project of klima, who also conducted the invocation and blessing of the wind turbines.</p>
+        <p>The wind farm uses 20 units of 70-metre (230 ft) high Vestas V82 1.65 MW wind turbines, arranged in a single row stretching along a nine-kilometer shoreline off Bangui Bay, facing the West Philippine Sea.</p>
+    </div>
+</body>    
+</html>
